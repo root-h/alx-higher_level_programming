60e3613c33b4d2bfea9ba71096e9c7a0c8f5b5b7
root-h <hannahmxo1.hm@gmail.com>
2022-05-31 21:32:58 +0000

    tasks

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+int main()
+listint_t *insert_node(listint_t **head, int number);
+
---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+
+listint_t *insert_node(listint_t **head, int number);
