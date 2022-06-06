3188ccb32ecbce51643d3751aeb223d88d1bfbcf
root-h <hannahmxo1.hm@gmail.com>
2022-06-06 21:25:32 +0000

    replace_el_atspecific_pstn

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def replace_in_list(my_list, idx, element): #replace element in list
     if 0 <= idx < len(my_list):
         my_list[idx] = element
     return (my_list)
