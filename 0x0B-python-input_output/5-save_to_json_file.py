a9335eefd8a3d6eaeec6811f77cfc3629ec72796
root-h <hannahmxo1.hm@gmail.com>
2022-07-06 01:05:18 +0300

    I/O

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Provides a function to write a JSON repr. of an object to a file"""
-
+"""
+writes an Object to a text file, using a JSON representation
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write the JSON representation of an object to a file"""
-    with open(filename, 'w') as ostream:
-        return json.dump(my_obj, ostream)
+    """
+    writes an Object to a text file, using a JSON representation
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""Provides a function to create an object from a JSON file"""
-
+"""
+creates an Object from a “JSON file”
+"""
 import json
 
 
 def load_from_json_file(filename):
-    """Create an object from a JSON file"""
-    with open(filename, 'r') as istream:
-        return json.load(istream)
+    """
+    creates an Object from a “JSON file”
+    """
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
